c39ff5b9ce83107bfbf6cd0df73f54f8eb245383
cheo0 <eliseo_0@ciencias.unam.mx>
2022-08-31 14:36:39 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+type Dog = {
+  id: number;
+  name: string;
+  age: number;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'prueba-angular';
+export class AppComponent implements OnInit {
+  dogs: Dog[] = [];
+
+  constructor(private readonly http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getDogs();
+  }
+
+  getDogs() {
+    this.http.get('http://localhost:3000/dogs').subscribe((value) => {
+      const dogs = value as Dog[];
+
+      this.dogs = dogs;
+    });
+  }
 }
